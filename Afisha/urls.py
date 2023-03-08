4621f5976c3ce236fa491d7373e51ec8d29e0363
imkr099 <imkr099@gmail.com>
2023-03-08 16:00:15 +0600

    'hw6'

--- Afisha/urls.py
+++ Afisha/urls.py
@@ -20,15 +20,15 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/directors/', views.director_list_api_view),
-    path('api/v1/directors/<int:id>/', views.director_detail_api_view),
-    path('api/v1/movies/', views.movie_list_api_view),
-    path('api/v1/movies/<int:id>/', views.movie_detail_api_view),
-    path('api/v1/reviews/', views.review_list_api_view),
-    path('api/v1/reviews/<int:id>/', views.review_detail_api_view),
-    path('api/v1/movies/reviews/', views.get_average),
-    path('api/v1/users/registration/', user_views.registration_view),
-    path('api/v1/users/authorization/', user_views.authorization_view),
-    path('api/v1/users/confirm/', user_views.confirm_view)
+    path('api/v1/directors/', views.DirectorListAPIView.as_view()),
+    path('api/v1/directors/<int:id>/', views.DirectorDetailAPIView.as_view()),
+    path('api/v1/movies/', views.MovieListAPIView.as_view()),
+    path('api/v1/movies/<int:id>/', views.MovieDetailAPIView.as_view()),
+    path('api/v1/reviews/', views.ReviewListAPIView.as_view()),
+    path('api/v1/reviews/<int:id>/', views.ReviewDetailAPIView.as_view()),
+    path('api/v1/movies/reviews/', views.GetAverage.as_view()),
+    path('api/v1/users/registration/', user_views.RegistrationAPIView.as_view()),
+    path('api/v1/users/authorization/', user_views.AuthorizationAPIView.as_view()),
+    path('api/v1/users/confirm/', user_views.ConfirmAPIView.as_view())
 ]
 
--- users/views.py
+++ users/views.py
@@ -6,44 +6,45 @@ from .serializers import UserValidationSerializer, UserLoginSerializer, ConfirmC
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from users.models import ConfirmCode
+from rest_framework.views import APIView
 import random
 
 
-@api_view(['POST'])
-def registration_view(request):
-    serializer = UserValidationSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    confirm_code = random.randint(100000, 999999)
-    user = User.objects.create_user(username=serializer.validated_data.get('username'),
-                                    password=serializer.validated_data.get('password'),
-                                    is_active=False)
-    ConfirmCode.objects.create(user=user, code=confirm_code)
-    return Response({"confirmation_code": confirm_code}, status=status.HTTP_201_CREATED)
+class RegistrationAPIView(APIView):
+    def post(self, request):
+        serializer = UserValidationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        confirm_code = random.randint(100000, 999999)
+        user = User.objects.create_user(username=serializer.validated_data.get('username'),
+                                        password=serializer.validated_data.get('password'),
+                                        is_active=False)
+        ConfirmCode.objects.create(user=user, code=confirm_code)
+        return Response({"confirmation_code": confirm_code}, status=status.HTTP_201_CREATED)
 
 
-@api_view(['POST'])
-def confirm_view(request):
-    serializer = ConfirmCodeSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    try:
-        confirm_code = ConfirmCode.objects.get(code=serializer.validated_data.get('code'))
-    except ConfirmCode.DoesNotExist:
-        return Response({"detail": "Invalid username or confirm code"}, status=status.HTTP_400_BAD_REQUEST)
-    user = confirm_code.user
-    user.is_active = True
-    user.save()
-    confirm_code.delete()
-    return Response({"message": "User registration confirmed successfully"}, status=status.HTTP_200_OK)
+class ConfirmAPIView(APIView):
+    def post(self, request):
+        serializer = ConfirmCodeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            confirm_code = ConfirmCode.objects.get(code=serializer.validated_data.get('code'))
+        except ConfirmCode.DoesNotExist:
+            return Response({"detail": "Invalid username or confirm code"}, status=status.HTTP_400_BAD_REQUEST)
+        user = confirm_code.user
+        user.is_active = True
+        user.save()
+        confirm_code.delete()
+        return Response({"message": "User registration confirmed successfully"}, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
-def authorization_view(request):
-    serializer = UserLoginSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    user = authenticate(username=serializer.validated_data.get('username'),
-                        password=serializer.validated_data.get('password'))
+class AuthorizationAPIView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(username=serializer.validated_data.get('username'),
+                            password=serializer.validated_data.get('password'))
 
-    if user:
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response(data={'key': token.key})
-    return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response(data={'key': token.key})
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
